5d07011bd090402209df6d1b5d013c3cfda946bd
nmachadoc1-ops <nmachadoc1@unicartagena.edu.co>
2026-09-22 18:28:47 -0500

    clase padre persona lista

---
+++ src/main/java/com/miapp/modelo/Persona.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.miapp.modelo;
+
+/**
+ *
+ * @author Estudiante
+ */
+public abstract class Persona {
+    
+    private String nombre;
+    protected int id;
+    
+    
+    public abstract double calcularPago();
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+    
+}
